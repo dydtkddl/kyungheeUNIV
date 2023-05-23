5e07726a6d22316a679fd44eb66499754e29dd3b
안용상 <dydtkddhkdwk@naver.com>
2023-05-23 18:18:50 +0900

    123

---
+++ 객체지향프로그램/정리/11강/11강/nullptr_delete.cpp
@@ -0,0 +1,17 @@
+/* #include <iostream>
+int main() {
+	int* p = nullptr;
+	if (p) {
+		std::cout << p << std::endl;
+	};
+	p = new int(10);
+	if (p) {
+		std::cout << p << std::endl;
+		std::cout << *p << std::endl;
+
+	};
+	delete p;
+	std::cout << p << std::endl;
+	std::cout << p << std::endl;
+	std::cout << *p << std::endl;
+}*/
