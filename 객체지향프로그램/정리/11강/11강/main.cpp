651b833549c6332ead340c5cb0ff1f353745e10d
안용상 <dydtkddhkdwk@naver.com>
2023-05-24 15:19:36 +0900

    123

--- 객체지향프로그램/정리/11강/11강/main.cpp
+++ 객체지향프로그램/정리/11강/11강/main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
-class Point {
-	int x, y;
-public:
-	Point(int x, int y) : x(x), y(y) {
-	};
-	Point* set(int x,int y) {
-		*this = { x , y };
-		return this;
+#include <vector>
+void print_v(std::vector<int> v) {
+	for (int i : v) {
+		std::cout << i << ',';
 	}
-	void print() const{
-		std::cout << x << ',' << y << std::endl;
-	};
-	void inc() {
-		x++;
-		y++;
+}
+void print_v2d(std::vector<std::vector<int>> v) {
+	for (std::vector<int> v_: v) {
+		for (int i : v_) {
+			std::cout << i << ',';
+		}
+		std::cout << std::endl;
 	}
-};
+}
 int main() {
-	Point p(1, 2); 
-	std::cout << p.set(1, 2) << std::endl;
-	std::cout << p.set(2, 3) << std::endl;
-	p.set(5, 6)->print();
-	p.inc();
-	p.print();
+	std::vector<int> a;
+	std::vector<int> a1(10);
+	std::vector<int>a2 = { 1,2,3,45 };
+	std::vector<std::vector<int>> v = { {1,2},{2,3} };
+	print_v2d(v);
+	print_v(a);
+	print_v(a1);
+	print_v(a2);
+	a1.assign(10, 32);
+	print_v(a1);
+	std::cout << "aÇÁ¸°Æ®" << std::endl;
+	print_v(a);
+	a.assign(12, 999);
+	print_v(a);
 }
